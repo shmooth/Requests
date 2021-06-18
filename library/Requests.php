09e07df5c043cc78ea79f46eca7c7d6f578d12e1
jrfnl <jrfnl@users.noreply.github.com>
2021-06-18 11:31:54 +0200

    Add Requests::has_capability() which can be used to determine if SSL is available.
    _Note: this is a recreation of PR 251, which was closed due to the branch rename and the remote no longer existing._
    
    Add `Requests::has_capability()` which can be used to determine if SSL is available.
    
    An example of it's usage: `Requests::has_capability( array( 'ssl' => true ) );`
    
    Co-authored-by: Dion Hulse <contact@dd32.id.au>

--- library/Requests.php
+++ library/Requests.php
@@ -219,6 +219,21 @@ class Requests {
 		return new $class();
 	}
 
+	/**
+	 * Checks to see if we have a transport for the capabilities requested.
+	 *
+	 * @return bool
+	 */
+	public static function has_capability($capabilities = array()) {
+		try {
+			$transport = self::get_transport($capabilities);
+		} catch (Requests_Exception $e) {
+			return false;
+		}
+
+		return true;
+	}
+
 	/**#@+
 	 * @see request()
 	 * @param string $url
