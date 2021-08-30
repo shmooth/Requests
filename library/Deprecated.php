b9e3aa18fa3f766ee1a24ed707f9472eda8f6bbe
jrfnl <jrfnl@users.noreply.github.com>
2021-08-30 03:46:43 +0200

    Deprecated classes classmap file: wrap each class/interface declaration in an "exists" check
    This fixes a potential fatal "class already exists" error when during one "page load":
    * Two projects both using Requests are used.
    * Neither of these projects uses Requests with a custom namespace/prefix as can be done via PHP_Scoper.
    * The first of these projects loaded is still using Requests 1.x and is using Composer autoload.
    * The second project is using Requests 2.x and is also using Composer autoload,
    
    In that case, depending on the order of the registered autoloaders, the `Deprecated.php` file _may_ be called while _some_ Requests 1.x have already been autoloaded, but not all.
    
    Bulk declaring the deprecated classes will then lead to a "Fatal error: Cannot declare interface/class Requests_..., because the name is already in use" error.
    
    Using `class_exists()` wrappers will prevent this.

--- library/Deprecated.php
+++ library/Deprecated.php
@@ -27,63 +27,177 @@ if (!defined('REQUESTS_SILENCE_PSR0_DEPRECATIONS') || REQUESTS_SILENCE_PSR0_DEPR
 	);
 }
 
-interface Requests_Auth extends Requests\Auth {}
-interface Requests_Hooker extends Requests\Hooker {}
-interface Requests_Proxy extends Requests\Proxy {}
-interface Requests_Transport extends Requests\Transport {}
+if (interface_exists('Requests_Auth') === false) {
+	interface Requests_Auth extends Requests\Auth {}
+}
+if (interface_exists('Requests_Hooker') === false) {
+	interface Requests_Hooker extends Requests\Hooker {}
+}
+if (interface_exists('Requests_Proxy') === false) {
+	interface Requests_Proxy extends Requests\Proxy {}
+}
+if (interface_exists('Requests_Transport') === false) {
+	interface Requests_Transport extends Requests\Transport {}
+}
 
-class Requests_Cookie extends Requests\Cookie {}
-class Requests_Exception extends Requests\Exception {}
-class Requests_Hooks extends Requests\Hooks {}
-class Requests_IDNAEncoder extends Requests\IdnaEncoder {}
-class Requests_IPv6 extends Requests\Ipv6 {}
-class Requests_IRI extends Requests\Iri {}
-class Requests_Response extends Requests\Response {}
-class Requests_Session extends Requests\Session {}
-class Requests_SSL extends Requests\Ssl {}
+if (class_exists('Requests_Cookie') === false) {
+	class Requests_Cookie extends Requests\Cookie {}
+}
+if (class_exists('Requests_Exception') === false) {
+	class Requests_Exception extends Requests\Exception {}
+}
+if (class_exists('Requests_Hooks') === false) {
+	class Requests_Hooks extends Requests\Hooks {}
+}
+if (class_exists('Requests_IDNAEncoder') === false) {
+	class Requests_IDNAEncoder extends Requests\IdnaEncoder {}
+}
+if (class_exists('Requests_IPv6') === false) {
+	class Requests_IPv6 extends Requests\Ipv6 {}
+}
+if (class_exists('Requests_IRI') === false) {
+	class Requests_IRI extends Requests\Iri {}
+}
+if (class_exists('Requests_Response') === false) {
+	class Requests_Response extends Requests\Response {}
+}
+if (class_exists('Requests_Session') === false) {
+	class Requests_Session extends Requests\Session {}
+}
+if (class_exists('Requests_SSL') === false) {
+	class Requests_SSL extends Requests\Ssl {}
+}
 
-class Requests_Auth_Basic extends Requests\Auth\Basic {}
-class Requests_Cookie_Jar extends Requests\Cookie\Jar {}
-class Requests_Proxy_HTTP extends Requests\Proxy\Http {}
-class Requests_Response_Headers extends Requests\Response\Headers {}
-class Requests_Transport_cURL extends Requests\Transport\Curl {}
-class Requests_Transport_fsockopen extends Requests\Transport\Fsockopen {}
-class Requests_Utility_CaseInsensitiveDictionary extends Requests\Utility\CaseInsensitiveDictionary {}
-class Requests_Utility_FilteredIterator extends Requests\Utility\FilteredIterator {}
+if (class_exists('Requests_Auth_Basic') === false) {
+	class Requests_Auth_Basic extends Requests\Auth\Basic {}
+}
+if (class_exists('Requests_Cookie_Jar') === false) {
+	class Requests_Cookie_Jar extends Requests\Cookie\Jar {}
+}
+if (class_exists('Requests_Proxy_HTTP') === false) {
+	class Requests_Proxy_HTTP extends Requests\Proxy\Http {}
+}
+if (class_exists('Requests_Response_Headers') === false) {
+	class Requests_Response_Headers extends Requests\Response\Headers {}
+}
+if (class_exists('Requests_Transport_cURL') === false) {
+	class Requests_Transport_cURL extends Requests\Transport\Curl {}
+}
+if (class_exists('Requests_Transport_fsockopen') === false) {
+	class Requests_Transport_fsockopen extends Requests\Transport\Fsockopen {}
+}
+if (class_exists('Requests_Utility_CaseInsensitiveDictionary') === false) {
+	class Requests_Utility_CaseInsensitiveDictionary extends Requests\Utility\CaseInsensitiveDictionary {}
+}
+if (class_exists('Requests_Utility_FilteredIterator') === false) {
+	class Requests_Utility_FilteredIterator extends Requests\Utility\FilteredIterator {}
+}
 
-class Requests_Exception_HTTP extends Requests\Exception\Http {}
-class Requests_Exception_Transport extends Requests\Exception\Transport {}
-class Requests_Exception_Transport_cURL extends Requests\Exception\Transport\Curl {}
-class Requests_Exception_HTTP_304 extends Requests\Exception\Http\Status304 {}
-class Requests_Exception_HTTP_305 extends Requests\Exception\Http\Status305 {}
-class Requests_Exception_HTTP_306 extends Requests\Exception\Http\Status306 {}
-class Requests_Exception_HTTP_400 extends Requests\Exception\Http\Status400 {}
-class Requests_Exception_HTTP_401 extends Requests\Exception\Http\Status401 {}
-class Requests_Exception_HTTP_402 extends Requests\Exception\Http\Status402 {}
-class Requests_Exception_HTTP_403 extends Requests\Exception\Http\Status403 {}
-class Requests_Exception_HTTP_404 extends Requests\Exception\Http\Status404 {}
-class Requests_Exception_HTTP_405 extends Requests\Exception\Http\Status405 {}
-class Requests_Exception_HTTP_406 extends Requests\Exception\Http\Status406 {}
-class Requests_Exception_HTTP_407 extends Requests\Exception\Http\Status407 {}
-class Requests_Exception_HTTP_408 extends Requests\Exception\Http\Status408 {}
-class Requests_Exception_HTTP_409 extends Requests\Exception\Http\Status409 {}
-class Requests_Exception_HTTP_410 extends Requests\Exception\Http\Status410 {}
-class Requests_Exception_HTTP_411 extends Requests\Exception\Http\Status411 {}
-class Requests_Exception_HTTP_412 extends Requests\Exception\Http\Status412 {}
-class Requests_Exception_HTTP_413 extends Requests\Exception\Http\Status413 {}
-class Requests_Exception_HTTP_414 extends Requests\Exception\Http\Status414 {}
-class Requests_Exception_HTTP_415 extends Requests\Exception\Http\Status415 {}
-class Requests_Exception_HTTP_416 extends Requests\Exception\Http\Status416 {}
-class Requests_Exception_HTTP_417 extends Requests\Exception\Http\Status417 {}
-class Requests_Exception_HTTP_418 extends Requests\Exception\Http\Status418 {}
-class Requests_Exception_HTTP_428 extends Requests\Exception\Http\Status428 {}
-class Requests_Exception_HTTP_429 extends Requests\Exception\Http\Status429 {}
-class Requests_Exception_HTTP_431 extends Requests\Exception\Http\Status431 {}
-class Requests_Exception_HTTP_500 extends Requests\Exception\Http\Status500 {}
-class Requests_Exception_HTTP_501 extends Requests\Exception\Http\Status501 {}
-class Requests_Exception_HTTP_502 extends Requests\Exception\Http\Status502 {}
-class Requests_Exception_HTTP_503 extends Requests\Exception\Http\Status503 {}
-class Requests_Exception_HTTP_504 extends Requests\Exception\Http\Status504 {}
-class Requests_Exception_HTTP_505 extends Requests\Exception\Http\Status505 {}
-class Requests_Exception_HTTP_511 extends Requests\Exception\Http\Status511 {}
-class Requests_Exception_HTTP_Unknown extends Requests\Exception\Http\StatusUnknown {}
+if (class_exists('Requests_Exception_HTTP') === false) {
+	class Requests_Exception_HTTP extends Requests\Exception\Http {}
+}
+if (class_exists('Requests_Exception_Transport') === false) {
+	class Requests_Exception_Transport extends Requests\Exception\Transport {}
+}
+if (class_exists('Requests_Exception_Transport_cURL') === false) {
+	class Requests_Exception_Transport_cURL extends Requests\Exception\Transport\Curl {}
+}
+if (class_exists('Requests_Exception_HTTP_304') === false) {
+	class Requests_Exception_HTTP_304 extends Requests\Exception\Http\Status304 {}
+}
+if (class_exists('Requests_Exception_HTTP_305') === false) {
+	class Requests_Exception_HTTP_305 extends Requests\Exception\Http\Status305 {}
+}
+if (class_exists('Requests_Exception_HTTP_306') === false) {
+	class Requests_Exception_HTTP_306 extends Requests\Exception\Http\Status306 {}
+}
+if (class_exists('Requests_Exception_HTTP_400') === false) {
+	class Requests_Exception_HTTP_400 extends Requests\Exception\Http\Status400 {}
+}
+if (class_exists('Requests_Exception_HTTP_401') === false) {
+	class Requests_Exception_HTTP_401 extends Requests\Exception\Http\Status401 {}
+}
+if (class_exists('Requests_Exception_HTTP_402') === false) {
+	class Requests_Exception_HTTP_402 extends Requests\Exception\Http\Status402 {}
+}
+if (class_exists('Requests_Exception_HTTP_403') === false) {
+	class Requests_Exception_HTTP_403 extends Requests\Exception\Http\Status403 {}
+}
+if (class_exists('Requests_Exception_HTTP_404') === false) {
+	class Requests_Exception_HTTP_404 extends Requests\Exception\Http\Status404 {}
+}
+if (class_exists('Requests_Exception_HTTP_405') === false) {
+	class Requests_Exception_HTTP_405 extends Requests\Exception\Http\Status405 {}
+}
+if (class_exists('Requests_Exception_HTTP_406') === false) {
+	class Requests_Exception_HTTP_406 extends Requests\Exception\Http\Status406 {}
+}
+if (class_exists('Requests_Exception_HTTP_407') === false) {
+	class Requests_Exception_HTTP_407 extends Requests\Exception\Http\Status407 {}
+}
+if (class_exists('Requests_Exception_HTTP_408') === false) {
+	class Requests_Exception_HTTP_408 extends Requests\Exception\Http\Status408 {}
+}
+if (class_exists('Requests_Exception_HTTP_409') === false) {
+	class Requests_Exception_HTTP_409 extends Requests\Exception\Http\Status409 {}
+}
+if (class_exists('Requests_Exception_HTTP_410') === false) {
+	class Requests_Exception_HTTP_410 extends Requests\Exception\Http\Status410 {}
+}
+if (class_exists('Requests_Exception_HTTP_411') === false) {
+	class Requests_Exception_HTTP_411 extends Requests\Exception\Http\Status411 {}
+}
+if (class_exists('Requests_Exception_HTTP_412') === false) {
+	class Requests_Exception_HTTP_412 extends Requests\Exception\Http\Status412 {}
+}
+if (class_exists('Requests_Exception_HTTP_413') === false) {
+	class Requests_Exception_HTTP_413 extends Requests\Exception\Http\Status413 {}
+}
+if (class_exists('Requests_Exception_HTTP_414') === false) {
+	class Requests_Exception_HTTP_414 extends Requests\Exception\Http\Status414 {}
+}
+if (class_exists('Requests_Exception_HTTP_415') === false) {
+	class Requests_Exception_HTTP_415 extends Requests\Exception\Http\Status415 {}
+}
+if (class_exists('Requests_Exception_HTTP_416') === false) {
+	class Requests_Exception_HTTP_416 extends Requests\Exception\Http\Status416 {}
+}
+if (class_exists('Requests_Exception_HTTP_417') === false) {
+	class Requests_Exception_HTTP_417 extends Requests\Exception\Http\Status417 {}
+}
+if (class_exists('Requests_Exception_HTTP_418') === false) {
+	class Requests_Exception_HTTP_418 extends Requests\Exception\Http\Status418 {}
+}
+if (class_exists('Requests_Exception_HTTP_428') === false) {
+	class Requests_Exception_HTTP_428 extends Requests\Exception\Http\Status428 {}
+}
+if (class_exists('Requests_Exception_HTTP_429') === false) {
+	class Requests_Exception_HTTP_429 extends Requests\Exception\Http\Status429 {}
+}
+if (class_exists('Requests_Exception_HTTP_431') === false) {
+	class Requests_Exception_HTTP_431 extends Requests\Exception\Http\Status431 {}
+}
+if (class_exists('Requests_Exception_HTTP_500') === false) {
+	class Requests_Exception_HTTP_500 extends Requests\Exception\Http\Status500 {}
+}
+if (class_exists('Requests_Exception_HTTP_501') === false) {
+	class Requests_Exception_HTTP_501 extends Requests\Exception\Http\Status501 {}
+}
+if (class_exists('Requests_Exception_HTTP_502') === false) {
+	class Requests_Exception_HTTP_502 extends Requests\Exception\Http\Status502 {}
+}
+if (class_exists('Requests_Exception_HTTP_503') === false) {
+	class Requests_Exception_HTTP_503 extends Requests\Exception\Http\Status503 {}
+}
+if (class_exists('Requests_Exception_HTTP_504') === false) {
+	class Requests_Exception_HTTP_504 extends Requests\Exception\Http\Status504 {}
+}
+if (class_exists('Requests_Exception_HTTP_505') === false) {
+	class Requests_Exception_HTTP_505 extends Requests\Exception\Http\Status505 {}
+}
+if (class_exists('Requests_Exception_HTTP_511') === false) {
+	class Requests_Exception_HTTP_511 extends Requests\Exception\Http\Status511 {}
+}
+if (class_exists('Requests_Exception_HTTP_Unknown') === false) {
+	class Requests_Exception_HTTP_Unknown extends Requests\Exception\Http\StatusUnknown {}
+}
